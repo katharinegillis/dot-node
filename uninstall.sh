ffce741df9b4e55966909c24be50cd517836b00b
Katharine Cordes <gillis.katharine@gmail.com>
2022-01-13 11:31:34 -0700

    chore: fix npm missing issue with nvm uninstall/install

--- uninstall.sh
+++ uninstall.sh
@@ -9,6 +9,7 @@ if [ "$SYSTEM" != "mac" ]; then
     # Remove yarn using apt
     sudo apt remove yarn -y
 else
+    source ~/.nvm/nvm.sh
     for v in $(nvm_ls); do nvm uninstall $v; done
     rm -rf "$NVM_DIR"
 fi
