4c5af87e29d9a75d1a3829d3d81659201f99c1f8
Katharine Cordes <gillis.katharine@gmail.com>
2022-02-07 10:08:54 -0700

    refactor: change from brew to native installer

--- install.sh
+++ install.sh
@@ -16,8 +16,8 @@ if [ "$SYSTEM" != "mac" ]; then
     # Install yarn
     sudo apt install yarn -y
 else
-    # Set up to use nvm
-    brew install node@"$NODE_VERSION"
+    # Set up
+    curl "https://nodejs.org/dist/latest/node-${NODE_VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
 
     # Install yarn
     npm install -g corepack
