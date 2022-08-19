3d68ac12b9983582398a8f092a4b3992b99855b5
Katharine Cordes <gillis.katharine@gmail.com>
2022-08-18 23:57:19 -0600

    refactor: switch to dockerized node
    Re-add the original dockerized node scripts and commands.
    
    Repo: dot-node
    Refs: DF-4

--- update.sh
+++ update.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-# Update node using apt
-sudo apt install nodejs -y
-
-# Update yarn using apt
-sudo apt install yarn -y
+# Pull latest node docker image
+docker pull node:lts
