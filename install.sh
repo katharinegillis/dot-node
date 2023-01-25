d30abce9f764be4d69747aba33f7806d781d48e8
Katharine Cordes <katharinegillis@users.noreply.github.com>
2023-01-25 00:03:52 -0700

    refactor: local install using nvm

--- install.sh
+++ install.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
 
-# Pull latest node docker image
-docker pull node:lts
+if ! [ -e "$HOME/.nvm" ]; then
+    NEW_NVM = 1;
+fi
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+echo "$NVM_DIR"
+
+if [ -z "$NEW_NVM" ]; then
+	nvm install --lts
+	nvm alias default 'lts/*'
+else
+	nvm use --lts
+	LTS_VERSION="$(nvm current)"
+	nvm install --lts
+	nvm reinstall-packages "$LTS_VERSION"
+	nvm use default
+fi
